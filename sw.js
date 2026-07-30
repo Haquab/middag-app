5b397c71c3d1c27d1c301d8041d23bae4edeaf84
Haquab <hakon.andre.bjercke@gmail.com>
2026-07-30 19:37:03 +0200

    endret service worker

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
-// Denne filen gjør ingenting mer enn å registrere seg selv.
-// Det holder for at iPhone skal tillate "Legg til på Hjem-skjerm"
-// som en fullverdig app. Du kan bygge ut offline-lagring her senere
-// hvis du vil.
-
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-  self.clients.claim();
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
